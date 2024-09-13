fb0aebe3e52d4888aaeba0392a5c2598aeb0eb51
Victor Anderssén <victor.anderssen@gmail.com>
2024-09-13 16:32:18 +0300

    Removed empty useEffect

--- src/features/homepage/home.tsx
+++ src/features/homepage/home.tsx
@@ -1,13 +1,8 @@
 import { Link } from "react-router-dom"
 import { useAppSelector } from "../../hooks";
-import { useEffect } from "react";
 
 export const HomePage = () => {
     const user = useAppSelector((state) => state.authentication.user);
-
-    useEffect(() => {
-
-    }, [user, user?.email]);
 
     return (
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
